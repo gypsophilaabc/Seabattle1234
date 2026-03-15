d82d2c24d961121fe70b4c0ffc2de7a07f4c4deb
dyh2026 <georgetyh@163.com>
2026-03-15 13:31:23 +0800

    提交Unity项目

--- SeaBattle1234/Assets/Scripts/Core/SceneLoader.cs
+++ SeaBattle1234/Assets/Scripts/Core/SceneLoader.cs
@@ -7,4 +7,9 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene("Scene_Battle");
     }
+    // 添加这个新方法用于加载战前配置场景
+    public void LoadPreBattleConfig()
+    {
+        SceneManager.LoadScene("Scene_PreBattleConfig");
+    }
 }
